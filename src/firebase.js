ec3635f0f87313663938ddd312db73e78d35e9b1
Richa Gupta <85608709+richagupta120698@users.noreply.github.com>
2024-04-29 15:37:00 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtAQDyxSJd6wb1zW9xLlvWSSqhDNKPOsA",
+    authDomain: "clone-19437.firebaseapp.com",
+    projectId: "clone-19437",
+    storageBucket: "clone-19437.appspot.com",
+    messagingSenderId: "775588100207",
+    appId: "1:775588100207:web:7b34c8870047ba007ab59a"
+  };
